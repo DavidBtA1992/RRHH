b002c571ea52b7ea72570c390f1934a813fb3b03
David Calderon <dfcalderong@unal.edu.co>
2024-03-25 20:10:36 -0500

    Second Commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
+var express = require("express");
+const { body } = require("express-validator");
+const Empleado = require("../public/javascripts/classes");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express'});
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/", function (req, res, next) {
+  res.send("done")
+  let nuevoEmpleado = new Empleado(
+    req.body.id,
+    req.body.nombre,
+    req.body.apellido,
+    req.body.salario,
+    req.body.eps,
+    req.body.arl,
+    req.body.fin,
+    req.body.feg,
+    req.body.fna
+  );
 });
 
 module.exports = router;
